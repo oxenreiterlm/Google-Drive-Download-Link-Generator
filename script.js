181bb5f0ec8f3ce78f793244c9be3243cdd255cf
oxenreiterlm <86936353+oxenreiterlm@users.noreply.github.com>
2021-10-04 15:23:37 -0400

    Completed JS codes for the rest of the windows

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ btn.addEventListener("click", createLink);
 
 function createLink(event) {
   event.preventDefault();
-  const gLinkValue = gLink.value;
   const confirmLink = gLink.value.includes("https://drive.google.com/file/d");
   if (confirmLink == true) {
     const getDownloadLink = gLink.value
@@ -19,17 +18,38 @@ function createLink(event) {
     downloadLink.value = getDownloadLink;
 
     function copyText(target) {
-      if (target.value == "") {
-        alert("Please insert a link to convert");
+      if (target.value == " ") {
+        alert("Please enter a link");
       } else {
         target.select();
         document.execCommand("copy");
-        alert("Link has been copied to clipboard");
       }
     }
     const copy = document.querySelector(".copy");
     copy.addEventListener("click", () => {
       return copyText(downloadLink);
     });
+    const audio1 = `<audio width="300" height="32" controls="controls" src="`;
+    const audio2 = `" type="audio/mp3"></audio>`;
+    const embedAudio = document.querySelector("#embed-audio");
+    embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
+
+    const copyAudio = document.querySelector(".copy-audio");
+    copyAudio.addEventListener("click", () => {
+      return copyText(embedAudio);
+    });
+
+    const getVideoLink = gLink.value.replace("/view?usp=sharing", "");
+    const video1 = `"<iframe src="`;
+    const video2 = `/preview width="560" height="315"></iframe>`;
+    const embedVideo = document.querySelector("#embed-video");
+    embedVideo.value = `${video1}${getVideoLink}${video2}`;
+
+    const copyVideo = document.querySelector(".copy-video");
+    copyVideo.addEventListener("click", () => {
+      return copyText(embedVideo);
+    });
+  } else {
+    alert("Please enter a Google Drive link");
   }
 }
